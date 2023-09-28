a312fce682f83eb2ece53bb6cebe0b11353ea122
mdosmangoni6355 <mdosmangoni6355@gmail.com>
2023-09-29 01:44:50 +0600

    some changes

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,6 +4,7 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
@@ -11,13 +12,29 @@ const Countries = () => {
         .then(data => setCountries(data))
     })
 
+    const handleVisitedCountry = country =>{
+        console.log('Add this to your visited country');
+        console.log(country);
+        const newCountries = [...visitedCountries, country];
+        setVisitedCountries(newCountries)
+    }
+
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+            <div>
+                <h3>Visited Countries: {visitedCountries.length}</h3>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
             <div className="country-container">
             {
                 countries.map(country => <Country 
                     key={country.cca3} 
+                    handleVisitedCountry={handleVisitedCountry}
                     country={country}></Country>)
             }
             </div>
